e80c95d2e8100dab694b3e2048d3245e1a5a4bc0
Kandresdiaz <kevina0416@gmail.com>
2026-08-27 04:05:24 -0500

    chore: cleanup test files

--- backend/src/test_ping.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-
-https.get('https://bot-whatsaap-tkjd.onrender.com/ping', (res) => {
-  let body = '';
-  res.on('data', chunk => body += chunk);
-  res.on('end', () => {
-    console.log('PING STATUS:', res.statusCode, 'BODY:', body);
-  });
-});
